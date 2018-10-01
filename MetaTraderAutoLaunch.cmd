17b24bb24e7d7d2b26a53481a4a1fd13ba499205
pplanb <rolandhablutzel@hotmail.com>
2018-10-01 10:23:54 +0100

    updated

--- MetaTraderAutoLaunch.cmd
+++ MetaTraderAutoLaunch.cmd
@@ -7,7 +7,5 @@ ping localhost -n 30
 start "1" "C:\Program Files (x86)\FxPro - Terminal1\terminal.exe" /portable
 start "2" "C:\Program Files (x86)\FxPro - Terminal2\terminal.exe" /portable
 start "3" "C:\Program Files (x86)\FxPro - Terminal3\terminal.exe" /portable
-start "4" "C:\Program Files (x86)\FxPro - Terminal4\terminal.exe" /portable
-start "5" "C:\Program Files (x86)\FxPro - Terminal5\terminal.exe" /portable
 
 exit
