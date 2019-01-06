b2d636a35347045758ae7dc2e5da5b6ddff52dfb
pplanb <rolandhablutzel@hotmail.com>
2019-01-06 20:03:41 +0100

    only keep 2 terminals

--- MetaTraderAutoLaunch.cmd
+++ MetaTraderAutoLaunch.cmd
@@ -5,7 +5,7 @@ rem *************************************************
 ping localhost -n 30
 
 start "1" "D:\FxPro - Terminal1\terminal.exe" /portable
-start "2" "D:\FxPro - Terminal2\terminal.exe" /portable
+rem start "2" "D:\FxPro - Terminal2\terminal.exe" /portable
 start "3" "D:\FxPro - Terminal3\terminal.exe" /portable
 
 exit
